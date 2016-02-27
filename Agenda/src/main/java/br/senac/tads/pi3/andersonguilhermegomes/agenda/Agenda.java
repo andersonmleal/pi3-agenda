4b7132a08a9f16c224e5c7a64cc1e6345bc0f452
anderson.mleal <anderson.mleal@CAS419299W0704.senacsp.edu.br>
2016-02-26 22:31:29 -0300

    instalação da dependencia JDBC

--- Agenda/src/main/java/br/senac/tads/pi3/andersonguilhermegomes/agenda/Agenda.java
+++ Agenda/src/main/java/br/senac/tads/pi3/andersonguilhermegomes/agenda/Agenda.java
@@ -5,11 +5,28 @@
  */
 package br.senac.tads.pi3.andersonguilhermegomes.agenda;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author anderson.mleal
  */
 public class Agenda {
+
+    private Connection obterConexao() throws SQLException, ClassNotFoundException {
+        Connection conn = null;
+
+        //passo1: registrar driver JDBC
+        Class.forName("org.apache.derby.jdbc.ClientDataSource");
+
+        //passo 2: Abrir conexão
+        conn = DriverManager.getConnection("jdbc:derby://localhost:1527/agendabd;SecurityMechanism=3",
+        "app", // usuario
+    "app"); // senha
     
-    
+    return conn;
+    }
+
 }
